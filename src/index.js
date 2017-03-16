4d1171f06007cb4dfba1349d37c35052f9a9e356
Kenni Gandira <Kenni Gandira>
2017-03-16 21:25:30 +0700

    lesson two

--- src/index.js
+++ src/index.js
@@ -3,7 +3,98 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// function Comment(props) {
+//   return (
+//     <div className="Comment">
+//       <div className="UserInfo">
+//         <Avatar user={props.author} />
+//         <UserInfo user={props.author />
+//       </div>
+//       <div className="Comment-text">
+//         {props.text}
+//       </div>
+//       <div className="Comment-date">
+//         {formatDate(props.date)}
+//       </div>
+//     </div>
+//   );
+// }
+
+// function Avatar(props) {
+// 	return (
+// 		<img className="Avatar"
+// 			src={props.user.avatarUrl}
+// 			alt={props.user.name}
+// 		/>
+// 	);
+// }
+
+// function UserInfo(props) {
+// 	return (
+// 		<div className="UserInfo">
+// 			<Avatar user={props.user} />
+// 			<div className="UserInfo-name">
+// 				{props.user.name}
+// 			</div>
+// 		</div>
+// 	);
+// }
+
+// function Apps() {
+//   return (
+//     <div>
+//       <Welcome name="Sara" />
+//       <Welcome name="Cahal" />
+//       <Welcome name="Edite" />
+//     </div>
+//     <div>
+//     	<Comment
+//     </div>
+//   );
+// }
+
+// function Clock(props) {
+// 	return (
+// 		<div>
+// 			<h1>
+// 				Hello, world!
+// 			</h1>
+// 			<h2>
+// 				It is {props.date.toLocaleTimeString()}.
+// 			</h2>
+// 		</div>
+// 	)
+// }
+
+class Clock extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>
+					Hello, world!
+				</h1>
+				<h2>
+					It is {this.props.date.toLocaleTimeString()}.
+				</h2>
+			</div>
+		)
+	}
+}
+
+function tick() {
+	ReactDOM.render(
+		<Clock date={new Date()} />,
+		document.getElementById('root')
+	);
+}
+
+setInterval(tick, 1000);
+
+// ReactDOM.render(
+//   <Apps />,
+//   document.getElementById('root')
+// );
